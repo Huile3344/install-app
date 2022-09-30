de51f1a41b916ad0695dcb90c69b9e57b4b0dc8c
Huile <1161115494@qq.com>
2022-09-30 14:46:58 +0800

    k8s 容器 container runtime 安装

--- k8s/install/install-runtime.sh
+++ k8s/install/install-runtime.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+# 安装在最新版本示例 ./install-runtime.sh
+
+# 和 v1.20.5 的 kubeadm 依赖的版本对应的docker最高版本 19.03.15-3
+# 安装指定版本示例 ./install-runtime.sh 19.03.15-3
+
 source /opt/shell/log.sh
 
 set +e
+
+NUM_RELEASE="${1}"
 
 # kubernetes 容器运行时安装 https://kubernetes.io/zh/docs/setup/production-environment/container-runtimes/
 
@@ -21,7 +29,11 @@ echo_exec 'yum install -y yum-utils device-mapper-persistent-data lvm2'
 echo_exec 'yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo'
 
 ## 安装 docker
-echo_exec 'yum --allowerasing install -y docker-ce docker-ce-cli containerd.io || yum install -y docker-ce docker-ce-cli containerd.io'
+if [[ -n $NUM_RELEASE ]]; then
+  echo_exec 'yum --allowerasing install -y docker-ce-${NUM_RELEASE}.el7.x86_64 docker-ce-cli-${NUM_RELEASE}.el7.x86_64 containerd.io || yum install -y docker-ce-${NUM_RELEASE}.el7.x86_64 docker-ce-cli-${NUM_RELEASE}.el7.x86_64 containerd.io'
+else
+   echo_exec 'yum --allowerasing install -y docker-ce docker-ce-cli containerd.io || yum install -y docker-ce docker-ce-cli containerd.io'
+fi
 
 ## 设置开机启动docker，启动并验证docker
 echo_exec 'systemctl enable docker && systemctl start docker && docker run hello-world'
